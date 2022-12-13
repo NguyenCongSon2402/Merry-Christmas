b30f98b88529ffaf34a4d5b3abce86082376c0fa
nguyễn công sơn <91744029+NguyenCongSon2402@users.noreply.github.com>
2022-12-13 16:10:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Merry Christmas';
+const titleCard = 'Tặng em yêu';
+const contentCard = 'Chúc honey của anh 25/12 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
